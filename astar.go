08bc7fdd66931f6fa2da41a26e7a49f1d7e159db
Rocky <rainbow_jincheng@hotmail.com>
2021-05-12 19:27:36 +0800

    Update astar.go

--- astar.go
+++ astar.go
@@ -4,6 +4,7 @@ import (
 	"container/heap"
 	"fmt"
 	"math"
+	cs "xxx.com/rocky/proto"
 )
 
 type aStarNode struct {
@@ -147,4 +148,89 @@ func heuristic(a, b *Node, hchoice int) float64 {
 	xDist := float64(a.row - b.row)
 	yDist := float64(a.col - b.col)
 	return math.Sqrt(math.Pow(xDist, 2) + math.Pow(yDist, 2))
+}
+
+var row_count = (46 * 4) * 4 // 0.25/4/4  0.25每格 改成 0.25/4 每格
+var col_count = (66 * 4) * 4
+
+type Point struct {
+	X int
+	Y int
+}
+
+// 客户端位置坐标 转 服务器格子坐标
+func ClientPointToSeverPoint(cx, cy float64) (x, y int) {
+
+	var a float64 = cx / 0.25 * 4
+	var b float64 = cy / 0.25 * 4
+
+	// col := int(math.Ceil(a))
+	// row := int(math.Ceil(b))
+
+	col := int(math.Floor(a))
+	row := int(math.Floor(b))
+
+	// fmt.Printf("a:%v b:%v\n", a, b)
+	// fmt.Printf("row:%v col:%v\n", row, col)
+
+	if row == 0 {
+		row = row_count - 1
+	} else {
+		row = row_count - row
+	}
+	col--
+	if col < 0 {
+		col = 0
+	}
+
+	return col, row
+
+}
+
+// 服务器格子坐标 转 客户端位置坐标
+func SeverPointToClientPoint(x, y int) (cx, cy float64) {
+
+	cx = float64(x+1) * (0.25 / 4)
+	cy = float64(row_count)*(0.25/4) - float64(y)*(0.25/4)
+
+	return
+}
+
+// 客户端寻路坐标
+func ClientPathPoint(start, end Point, mBlocker map[string]int) (list []*cs.PB_XYZ) {
+
+	var rows = row_count
+	var cols = col_count
+	matrix := make([][]uint8, rows)
+	for i := 0; i < rows; i++ {
+		matrix[i] = make([]uint8, cols)
+	}
+
+	for y := 0; y < rows; y++ {
+		for x := 0; x < cols; x++ {
+
+			// 设置障碍
+			if _, ok := mBlocker[fmt.Sprintf("%d,%d", x, y)]; ok {
+				matrix[y][x] = 1
+				continue
+			}
+		}
+
+	}
+
+	path, err := AStarWithJump(matrix, GetNode(start.Y, start.X), GetNode(end.Y, end.X), 1)
+	if err != nil {
+		return nil
+	}
+	if len(path.Nodes) == 0 {
+		return nil
+	}
+
+	for _, node := range path.Nodes {
+		cx, cy := SeverPointToClientPoint(node.col, node.row)
+		item := &cs.PB_XYZ{X: float32(cx), Y: 10, Z: float32(cy)}
+		list = append(list, item)
+	}
+
+	return
 }
